55a0be5ec7c6682e7781b2cad61b5cf7ad3b03a3
Michael Balling <mballing@students.mchenry.edu>
2018-01-18 12:03:54 -0600

    Update Main.java

--- src/com/example/java/Main.java
+++ src/com/example/java/Main.java
@@ -3,8 +3,9 @@ package com.example.java;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello World!");
-	String aString = "Mike Balling";
-	System.out.println("My name is " + aString);
+        System.out.println("Hello, my name is Mike Balling.");
+        System.out.println("I am 30 years old, married, with our first baby on the way!");
+        System.out.println("My career as a pilot has taken me all around the country, and soon the world.");
+        System.out.println("Programming allows me to diversify my skill set while tinkering with technology.");
     }
 }
